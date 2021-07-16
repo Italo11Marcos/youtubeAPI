11438e7caf09ffaaf0064256dae108db3a8a4712
italo11marcos <italomarcosboa@gmail.com>
2021-07-16 18:56:51 -0300

    Revert "Melhora no código playlists.py"

--- playlists.py
+++ playlists.py
@@ -1,44 +1,32 @@
 from googleapiclient.discovery import build
 
-api_key = "AIzaSyAT5UBHH0QXhNN2olADNccvM_JDmMZ_Uyo"
-
-def playlists_channel(channel_id):
-
-    youtube = build("youtube", "v3", developerKey=api_key)
-
-    playlists_response = youtube.playlists().list(
-        part = "snippet",
-        channelId = channel_id,
-        maxResults = 50
-    ).execute()
-
-    playlists = []
-
-    nextPage_token = None
-
-    while True:
-        response = youtube.playlists().list(
-            part = "snippet",
-            channelId = channel_id,
-            maxResults = 50,
-            pageToken=nextPage_token
-        ).execute()
-
-        playlists += response['items']
-
-        nextPage_token = response.get('nestPageToken')
-
-        if nextPage_token is None:
-            break
-
-    playlists_ids = list(map(lambda x: x['id'], playlists))
-    #playlists_titles = list(map(lambda x: x['snippet']['title'], playlists))
-
-    return playlists_ids
-
-
 channel_id = "UCcF_6w2j1_jVyrvcouKB_NA"
 
-x = playlists_channel(channel_id)
+youtube = build("youtube", "v3", developerKey = "")
 
-print(x)
+playlists_response = youtube.playlists().list(
+    part = "snippet",
+    channelId = channel_id,
+    maxResults = 50
+).execute()
+
+playlists = []
+
+while playlists_response:
+
+    for item in playlists_response['items']:
+
+        id = item['id']
+        title = item['snippet']['title']
+        description = item['snippet']['description']
+
+        print(id, title, description)
+
+    # Se tiver próxima página, continua
+    if 'nextPageToken' in playlists_response:
+        playlists_response = youtube.playlists().list(
+                part = "snippet",
+                channelId = channel_id
+            ).execute()
+    else:
+        break
